e682b9b99a2fb91aacee9a4b2fabf074cfd9ec38
Rutuja Khade <khaderutuja17@gmail.com>
2026-03-04 23:25:07 +0530

    Added update and delete routes, fixed edit functionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose= require("mongoose");
 const Listing = require("./models/listing.js")
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -14,23 +16,72 @@ main().then(() =>{
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
     res.send("hi i am root");
+});
+app.get("/Listings", async(req, res) => {
+    const allListings = await Listing.find({});
+    res.render("Listings/index.ejs", {allListings});
+});
+
+//New Route
+ app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+ });
+
+//show route
+ app.get("/listings/:id", async (req, res) =>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+ });
+
+ //Create route
+app.post("/listings", async (req, res) =>{
+   const newListing = new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) =>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+//update route
+app.put("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
 })
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
+//Delet rute
+app.delete("/listings/:id", async(req, res) =>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-});
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 app.listen(8080, () => {
     console.log("server is listening to post 8080");
 });
