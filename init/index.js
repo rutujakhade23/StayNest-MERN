a972dbc7e2bfda5e5194fa7d28e8934a9f3fa1db
Rutuja Khade <khaderutuja17@gmail.com>
2026-05-19 11:09:26 +0530

    fix: remove EJS inside HTML comments

--- init/index.js
+++ init/index.js
@@ -1,28 +1,58 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+// const MONGO_URL = process.env.ATLASDB_URL;
+
+// main() 
+//     .then(() => {
+//         console.log("connected to DB");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+//     async function main() {
+//         await mongoose.connect(MONGO_URL);
+//     }
+
+//     const initDB = async() => {
+//         await Listing.deleteMany({});
+//         // initData.data = initData.data.map((obj) => ({
+//         //     ...obj, 
+//         //     owner:  "6a036d0f5bfff94bb70e5483"}));
+//         await Listing.insertMany(initData.data);
+//         console.log("data was initialized");
+//     };
+
+//     initDB();
+
+
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
-main() 
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+async function main() {
+    await mongoose.connect(MONGO_URL);
+    console.log("connected to DB");
 
-    async function main() {
-        await mongoose.connect(MONGO_URL);
-    }
+    await initDB();
+}
 
-    const initDB = async() => {
-        await Listing.deleteMany({});
-        initData.data = initData.data.map((obj) => ({
-            ...obj, 
-            owner:  "6a036d0f5bfff94bb70e5483"}));
-        await Listing.insertMany(initData.data);
-        console.log("data was initialized");
-    };
+main().catch((err) => {
+    console.log(err);
+});
 
-    initDB();
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
